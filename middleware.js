7ae1cd025c08b0962ac2dcfe46fd061d08173cd4
nishi kant kumar <nishikants192@gmail.com>
2026-02-11 17:58:35 +0530

    Implement admin/user listing page and add middleware for controlled user deletion

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const ExpressError = require("./utils/ExpressError");
+const User=require("./model/user.js")
 
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
@@ -9,6 +10,13 @@ module.exports.isLoggedIn=(req,res,next)=>{
 module.exports.isAdmin=(req,res,next)=>{
     if(req.user.role !== "admin"){
         return next(new ExpressError(403,"You don't have permission's to do this"));
+    }
+    next();
+}
+module.exports.isSuperAdmin=async(req,res,next)=>{
+    let user=await User.findOne({isSuperAdmin:true});
+    if(!user){
+        next(new ExpressError(403,"Permission Denied"));
     }
     next();
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,29 @@
 const express=require("express");
 const router=express.Router();
-const User=require("../model/user.js")
+const User=require("../model/user.js");
+const {isLoggedIn,isAdmin,isSuperAdmin}=require("../middleware.js");
 
-router.get("/ListOfAdmin",async(req,res)=>{
+router.get("/ListOfAdmin",isLoggedIn,isAdmin,isSuperAdmin,async(req,res)=>{
     const AdminList=await User.find({role:"admin"});
-    res.render("AdminFolder/admin.js",{AdminList});
+    res.render("AdminFolder/admin.ejs",{AdminList});
 })
+router.get("/update/:id",isLoggedIn,isAdmin,isSuperAdmin,async(req,res)=>{
+    const {id}=req.params;
+    const userDetails=await User.findById(id);
+    userDetails.role="user";
+    await userDetails.save();
+    res.redirect("/admin/ListOfAdmin");
+})
+router.get("/showUsers",isLoggedIn,isAdmin,async(req,res)=>{
+    const userDetails=await User.find({role:"user"});
+    res.render("AdminFolder/allUser.ejs",{userDetails});
+})
+router.get("/removeUser/:id",isLoggedIn,isAdmin,async(req,res)=>{
+    let {id}=req.params;
+    await User.findByIdAndDelete(id);
+    res.redirect("/admin/showUsers");
+})
+
+// TODO (Data Integrity): Implement cascading delete to remove user posts when a user is deleted
 
 module.exports=router;
